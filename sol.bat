eaf257d84342ade5efc19ef3230de235b193d02d
is2js <tingstyle1@gmail.com>
2022-08-11 17:59:49 +0900

    feat: sol.bat에 reset 인자 추가하여 연습후 reset할 수 있게 함

--- sol.bat
+++ sol.bat
@@ -46,7 +46,7 @@ if "%1" == "bj" (
         GOTO:EOF
     )
     Set CURRENT_FOLDER=others
-) else if "%1" == "cpt" (
+)  else if "%1" == "cpt" (
    ::concept폴더가 없으면 일단 생성한다.
    if not exist ".\concept\" md ".\concept"
    ::concept/ 첫번째인수명/ 으로 해당 문제의 폴더를 생성한다.
@@ -57,33 +57,57 @@ if "%1" == "bj" (
        GOTO:EOF
    )
    Set CURRENT_FOLDER=concept
+)  else if "%1" == "reset" (
+    GOTO:RESET
 )  else (
     echo "Must First arguments contains bj, pg, lc, others, cpt"
     GOTO:EOF
 )
 
-::Solution.py -> .\backjoon\첫번째인수명\첫번째인수명.py으로 복사한다
-if "%3" == "" copy %cd%\Solution.py .\%CURRENT_FOLDER%\%2\%2.py
+::solution.py -> .\backjoon\첫번째인수명\첫번째인수명.py으로 복사한다
+if "%3" == "" copy %cd%\solution.py .\%CURRENT_FOLDER%\%2\%2.py
 ::               만약, 2번째 인수가 존재한다면-> .\backjoon\첫번째인수명(2번째인수명).py으로 복사한다.
-if not "%3" == "" copy %cd%\Solution.py .\%CURRENT_FOLDER%\%2\%2(%3).py
+if not "%3" == "" copy %cd%\solution.py .\%CURRENT_FOLDER%\%2\%2(%3).py
 :: input, output.txt를 .\backjoon\첫번째인수명 폴더로 복사한다.
 copy %cd%\input.txt .\%CURRENT_FOLDER%\%2\input.txt
 copy %cd%\output.txt .\%CURRENT_FOLDER%\%2\output.txt
 
 
-::기존 Solution.py 초기문장 적어 초기화
-echo import sys > .\Solution.py
-echo. >> .\Solution.py
-echo input = sys.stdin.readline >> .\Solution.py
-echo. >> .\Solution.py
-echo. >> .\Solution.py
-echo def solution(): >> .\Solution.py
-echo     pass >> .\Solution.py
-echo. >> .\Solution.py
-echo. >> .\Solution.py
-echo if __name__ == '__main__': >> .\Solution.py
-echo     solution() >> .\Solution.py
+::기존 solution.py 초기문장 적어 초기화
+echo import sys > .\solution.py
+echo. >> .\solution.py
+echo input = sys.stdin.readline >> .\solution.py
+echo. >> .\solution.py
+echo. >> .\solution.py
+::echo def solution(): >> .\solution.py
+::echo     pass >> .\solution.py
+::echo. >> .\solution.py
+::echo. >> .\solution.py
+echo if __name__ == '__main__': >> .\solution.py
+echo      pass >> .\solution.py
+::echo     solution() >> .\solution.py
 
+:: 기존 input, output.txt를 빈 파일로 생성
+type NUL > .\input.txt
+type NUL > .\output.txt
+
+GOTO:EOF
+
+:: reset만 하는 함수
+:RESET
+::기존 solution.py 초기문장 적어 초기화
+echo import sys > .\solution.py
+echo. >> .\solution.py
+echo input = sys.stdin.readline >> .\solution.py
+echo. >> .\solution.py
+echo. >> .\solution.py
+::echo def solution(): >> .\solution.py
+::echo     pass >> .\solution.py
+::echo. >> .\solution.py
+::echo. >> .\solution.py
+echo if __name__ == '__main__': >> .\solution.py
+echo     pass >> .\solution.py
+::echo     solution() >> .\solution.py
 :: 기존 input, output.txt를 빈 파일로 생성
 type NUL > .\input.txt
 type NUL > .\output.txt
